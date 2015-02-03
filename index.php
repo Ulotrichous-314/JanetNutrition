2b73b0be6d92949d6e4e4495dfa57a68c62ae1dd
Will Ledsham <william@ledsham.org>
2015-02-03 22:13:32 +0000

    Added 'main' div to index.php

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
-<?php get_sidebar(); ?>
-    <div id="content">
-        <?php if(have_posts()):while(have_posts()):the_post();?>
-            <div <?php post_class() ?>>
-                <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                <?php the_excerpt(); ?>
-                <div class="post-meta">
-                    <?php jn_post_meta();?>
+    <div id="main">
+        <div id="content">
+            <?php if(have_posts()):while(have_posts()):the_post();?>
+                <div <?php post_class() ?>>
+                    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                    <?php the_excerpt(); ?>
+                    <div class="post-meta">
+                        <?php jn_post_meta();?>
+                    </div>
                 </div>
-            </div>
-            <?php endwhile; else: ?>
-        <h2><?php _e('Nothing Found','jn') ?></h2>
-        <p><?php _e('No posts were found. Sorry.','jn'); ?>
-        <a href="<?php echo get_option('home'); ?>"><?php _e('Return to homepage','jn') ?></a></p>
-        <?php endif; ?>
+                <?php endwhile; else: ?>
+            <h2><?php _e('Nothing Found','jn') ?></h2>
+            <p><?php _e('No posts were found. Sorry.','jn'); ?>
+            <a href="<?php echo get_option('home'); ?>"><?php _e('Return to homepage','jn') ?></a></p>
+            <?php endif; ?>
+        </div>
     </div>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
 
